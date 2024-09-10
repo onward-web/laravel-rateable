fefc170b961c37c6601fe315e2763c6a1cfca114
onward-web <developer@onward-web.pp.ua>
2024-09-10 19:37:14 +0300

    first

--- src/RatingDescription.php
+++ src/RatingDescription.php
@@ -7,6 +7,8 @@ class RatingDescription extends Model
 {
     use Filterable;
     protected $guarded = [];
+    public $timestamps = false;
+    public $incrementing = false;
 
     public $table = RATING_DESCRIPTION_TABLE_NAME;
 
