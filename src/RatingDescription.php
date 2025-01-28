560728a03b3d1d01b84298c27580d50f819eb9b5
onward-web <developer@onward-web.pp.ua>
2025-01-28 23:23:21 +0200

    Update RatingDescription.php

--- src/RatingDescription.php
+++ src/RatingDescription.php
@@ -5,12 +5,14 @@ use Illuminate\Database\Eloquent\Model;
 use EloquentFilter\Filterable;
 class RatingDescription extends Model
 {
+    use \Jeidison\CompositeKey\CompositeKey;
+
     use Filterable;
     protected $guarded = [];
     public $timestamps = false;
     public $incrementing = false;
 
-    protected $primaryKey = 'rating_id';
+    protected $primaryKey = ['rating_id', 'lang'];
 
     public $table = RATING_DESCRIPTION_TABLE_NAME;
 
